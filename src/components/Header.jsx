f8f3f90193d4da4689e59fdc6b85948e7a18535a
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-12-10 12:17:04 +0530

    add Avatar and DropdownMenu components in Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "@/assets/Logo.png";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  const user = true;
+
+  return (
+    <nav className="py-4 px-8 flex justify-between items-center">
+      <Link to="/">
+        <img src={Logo} className="h-16" alt="ClipIt Logo" />
+      </Link>
+
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                <span>My Links</span>
+                </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+}
