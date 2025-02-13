e17805ad496cc4e9ff9e07796bf30412abae9e8d
Leonardo Fernandes <113446491+Leonardo1903@users.noreply.github.com>
2025-02-13 23:47:18 +0530

    Update AppLayout.jsx

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,13 +4,13 @@ import { Outlet } from "react-router-dom";
 export default function AppLayout() {
   return (
     <>
-      <main className="min-h-screen container">
+      <main className="min-h-screen ">
         <Header />
         <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-gray-100">
           <Outlet />
         </div>
-      <Footer />
       </main>
+      <Footer />
     </>
   );
 }
